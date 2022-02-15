9fad79c62b65e764d46bd53f862394d03385b0ea
smmullin <smmullin@buffalo.edu>
2022-02-15 02:24:44 -0500

    Updated posts cell and finished crud operations

---
+++ Xccelerator/Xccelerator/CarPostsCell.swift
@@ -0,0 +1,23 @@
+//
+//  CarPostsCell.swift
+//  Xccelerator
+//
+//  Created by Sarah Mullings on 2/15/22.
+//
+
+import UIKit
+
+class CarPostsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
