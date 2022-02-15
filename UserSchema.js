148d65052562c87aa3ddcf4460a4a0198e181c02
kaylahharden <kaylahharden@gmail.com>
2022-02-15 00:52:48 -0500

    Updates user schema

--- UserSchema.js
+++ UserSchema.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema
 
-var userSchema = new Schema({                       //new object of user login info
+var userSchema = new Schema({                       //new datd object of user login info
     email: String,
     password: String
 });
 
 const User = mongoose.model("myuser", userSchema);   // creates a data object which is a userSchema
-module.exports = User;                               // allows us to access the cuserschema from the server.js file
+module.exports = myuser;                               // allows us to access the userschema from the server.js file
